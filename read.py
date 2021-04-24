e84bcf4d7e195841fb9218045c41352a3dcf4cd2
thom8047 <thom8047@bears.unco.edu>
2021-04-24 14:24:46 -0600

    Update py script

--- read.py
+++ read.py
@@ -1,14 +1,16 @@
 def getText():
-    location = "/home/pi/" + str(input("/home/pi/"))
+    location = str(input("/home/pi/"))
     file = open(location, "r");
     for line in file:
         split = line.split(" ")
         if (split[0].count("\t") > 1):
-            print(line)
+            holder = line.split("\n")
+            print(holder[0] + " | ", end="")
         elif (split[0].count("\t") > 0):
             print(line)
         else:
-            print(line)
+            if (line != "\n"):
+                print(line)
 
 if __name__ == "__main__":
     getText()
