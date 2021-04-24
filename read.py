dab51ffc18e5d500e0beda4903217e010fd081bd
thom8047 <thom8047@bears.unco.edu>
2021-04-24 14:45:58 -0600

    Update py script

--- read.py
+++ read.py
@@ -1,17 +1,24 @@
 def getText():
     location = "/home/pi/" + str(input("/home/pi/"))
     file = open(location, "r");
+    line_num = 0
     for line in file:
+        if (line_num > 10):
+            user_input = str(input())
+            if (user_input):
+                break;
+
         split = line.split(" ")
         if (split[0].count("\t") > 1):
-            holder = line.split("\n")
-            print(holder[0] + " | ", end="")
+            print(line)
         elif (split[0].count("\t") > 0):
             if (line.count("use case:") < 1):
                 print(line)
         else:
-            if (line == "\n"):
+            if (line != "\n"):
                 print(line)
+
+        line_num += 1;
 
 
 if __name__ == "__main__":
